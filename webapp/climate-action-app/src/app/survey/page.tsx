f7ca12e011a8051925b798c0028c88de333ea6ff
Bradley Kohler <bradleyk0205@gmail.com>
2025-04-17 14:39:40 +0200

    Add state for formData to store user's answers

--- webapp/climate-action-app/src/app/survey/page.tsx
+++ webapp/climate-action-app/src/app/survey/page.tsx
@@ -1,12 +1,40 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
+import { group } from "console";
 
 export default function Home() {
 
+    // State to manage user's input into form
+    const [formData, setFormData] = useState({
+        alias: "",                  // Used to identify the user with a custom name 
+        referredBy: "", // 
+        inclinationToChange: "",
+        largestImpactChocie: "",
+        carbonFootprint: 0,
+        willingToChange: [] as string[],
+        rankedWillingToChange: [
+            "Home",
+            "Electricity",
+            "Diet",
+            "Ground Transportation",
+            "Air Travel",
+            "Other Consumption",
+        ],
+        airTravelToGiveUp: [] as string[],
+        dietToGiveUp: "",
+        homeOrElectricityToGiveUp: "",
+        groundTransportationToGiveUp: "",
+        consumptionToGiveUp: "",
+        willingToEngageWith: [] as string[],
+        individualGoals: [] as string[],
+        groupGoals: [] as string[],
+    });
+
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            
+
             <Link href="/">
                 <button className="calc-btn">Home</button>
             </Link>
